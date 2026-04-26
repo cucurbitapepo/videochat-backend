860015f1fb5ddf62687f055b2fa3665fe6f62268
cucurbitapepo <anedav@yandex.ru>
2026-04-27 01:15:55 +0300

    fixed inputstream for FirebaseConfig

--- src/main/java/com/videochatapi/config/FirebaseConfig.java
+++ src/main/java/com/videochatapi/config/FirebaseConfig.java
@@ -6,18 +6,17 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
   @Bean
   public FirebaseApp firebaseApp() throws IOException {
-    FileInputStream serviceAccount = new FileInputStream(
-            getClass().getClassLoader().getResource("videochat-1e748-firebase-adminsdk-fbsvc-d72751fcd3.json").getFile()
-    );
+    InputStream serviceAccount = new ClassPathResource("videochat-1e748-firebase-adminsdk-fbsvc-d72751fcd3.json").getInputStream();
 
     FirebaseOptions options = FirebaseOptions.builder()
             .setCredentials(GoogleCredentials.fromStream(serviceAccount))
